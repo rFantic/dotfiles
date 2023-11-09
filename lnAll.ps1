7c5762e7d0f3a27f5136e63d189e660a6a0bd167
Nguyễn Hữu Nghĩa <nhnske@gmail.com>
2023-11-09 11:50:52 +0700

    add soft link script for windows

---
+++ lnAll.ps1
@@ -0,0 +1,4 @@
+ni -ItemType SymbolicLink -Path C:\Users\Admin\appdata\Local\nvim -Target C:\Users\ADMIN\dotfiles\nvim\
+ni -ItemType SymbolicLink -Path C:\Users\ADMIN\scoop\persist\win-vind\config\.vindrc -Target C:\Users\ADMIN\dotfiles\.vindrc
+ni -ItemType SymbolicLink -Path $PROFILE -Target C:\Users\ADMIN\dotfiles\Microsoft.PowerShell_profile.ps1
+ni -ItemType SymbolicLink -Path $env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json -Target C:\Users\ADMIN\dotfiles\wt\settings.json
