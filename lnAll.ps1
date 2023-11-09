15c15af05363389d6d58ae0de541ce9642fe5de3
Nguyễn Hữu Nghĩa <nhnske@gmail.com>
2023-11-09 13:20:18 +0700

    add code action keymap

--- lnAll.ps1
+++ lnAll.ps1
@@ -1,4 +1,4 @@
-ni -ItemType SymbolicLink -Path C:\Users\Admin\appdata\Local\nvim -Target C:\Users\ADMIN\dotfiles\nvim\
-ni -ItemType SymbolicLink -Path C:\Users\ADMIN\scoop\persist\win-vind\config\.vindrc -Target C:\Users\ADMIN\dotfiles\.vindrc
-ni -ItemType SymbolicLink -Path $PROFILE -Target C:\Users\ADMIN\dotfiles\Microsoft.PowerShell_profile.ps1
-ni -ItemType SymbolicLink -Path $env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json -Target C:\Users\ADMIN\dotfiles\wt\settings.json
+New-Item -ItemType SymbolicLink -Path C:\Users\Admin\appdata\Local\nvim -Target C:\Users\ADMIN\dotfiles\nvim\
+New-Item -ItemType SymbolicLink -Path C:\Users\ADMIN\scoop\persist\win-vind\config\.vindrc -Target C:\Users\ADMIN\dotfiles\.vindrc
+New-Item -ItemType SymbolicLink -Path $PROFILE -Target C:\Users\ADMIN\dotfiles\Microsoft.PowerShell_profile.ps1
+New-Item -ItemType SymbolicLink -Path $env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json -Target C:\Users\ADMIN\dotfiles\wt\settings.json
--- nvim/lua/keymap.lua
+++ nvim/lua/keymap.lua
@@ -10,4 +10,5 @@ nnoremap <leader>bd <cmd>bd! <cr>
 nnoremap <leader>rc <cmd>browse oldfiles <cr>
 nnoremap <leader>tm <cmd>term <cr>
 nnoremap <leader>id <cmd>put=strftime('%c') <cr>
+nnoremap <leader>ca <cmd>lua vim.lsp.buf.code_action()<CR>
 ]])
