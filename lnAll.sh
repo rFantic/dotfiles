e560b780ab87bb0c0e14f5f55b92df4e22e59d85
Nghia Nguyen <nghianh@horusvn.com>
2023-12-26 14:55:24 +0700

    Add mini sessions

--- lnAll.sh
+++ lnAll.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 ln -s  ~/dotfiles/nvim/ ~/.config/nvim
 ln -s  ~/dotfiles/.bashrc ~/.bashrc
+ln -s  ~/dotfiles/.tmux.conf ~/.tmux.conf
--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -6,6 +6,10 @@ return {
             require('mini.surround').setup()
             require('mini.pairs').setup()
             require('mini.jump').setup()
+            require('mini.sessions').setup({
+                file = '',
+                force = {delete = true},
+            })
         end,
     },
 }
