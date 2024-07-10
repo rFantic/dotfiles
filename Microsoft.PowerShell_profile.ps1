67408137da928966cb8de68d503b48e355b5c42f
rfantic <nhnske@gmail.com>
2024-07-10 23:03:57 +0700

    update windows config

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -7,6 +7,7 @@ function lsf {ls | Format-Wide -Column 4}
 function renplv {curl -X POST -d '{}' https://api.netlify.com/build_hooks/652d0cf2603f7a2d000d1c00} 
 Set-Alias -n lg -v lazygit
 Set-Alias -n mm -v micromamba
-$Env:MAMBA_ROOT_PREFIX='C:\Users\ADMIN\micromambaenv'
+$Env:MAMBA_ROOT_PREFIX='C:\condaenv'
 try {micromamba.exe shell hook -q -s powershell | out-string | invoke-expression 2>$null} catch {}
-Invoke-Expression (&starship init powershell)
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
+#Invoke-Expression (&starship init powershell)
--- lnAll.ps1
+++ lnAll.ps1
@@ -1,4 +1,8 @@
-New-Item -ItemType SymbolicLink -Path C:\Users\Admin\appdata\Local\nvim -Target C:\Users\ADMIN\dotfiles\nvim\
-New-Item -ItemType SymbolicLink -Path C:\Users\ADMIN\scoop\persist\win-vind\config\.vindrc -Target C:\Users\ADMIN\dotfiles\.vindrc
-New-Item -ItemType SymbolicLink -Path $PROFILE -Target C:\Users\ADMIN\dotfiles\Microsoft.PowerShell_profile.ps1
-New-Item -ItemType SymbolicLink -Path $env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json -Target C:\Users\ADMIN\dotfiles\wt\settings.json
+Remove-Item C:\Users\Admin\appdata\Local\nvim
+Remove-Item C:\Users\ADMIN\scoop\persist\win-vind\config\.vindrc
+Remove-Item $PROFILE
+#Remove-Item $env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
+New-Item -ItemType SymbolicLink -Path C:\Users\Admin\appdata\Local\nvim -Target C:\Home\dotfiles\nvim\
+New-Item -ItemType SymbolicLink -Path C:\Users\ADMIN\scoop\persist\win-vind\config\.vindrc -Target C:\Home\dotfiles\.vindrc
+New-Item -ItemType SymbolicLink -Path $PROFILE -Target C:\Home\dotfiles\Microsoft.PowerShell_profile.ps1
+#New-Item -ItemType SymbolicLink -Path $env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json -Target C:\Home\dotfiles\wt\settings.json
