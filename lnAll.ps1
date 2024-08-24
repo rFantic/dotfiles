0c915031f4f7a700c213044ec3fbeaf41e6ab408
rfantic <nhnske@gmail.com>
2024-08-24 21:25:37 +0700

    fix path lnAll.ps1

--- lnAll.ps1
+++ lnAll.ps1
@@ -1,8 +1,8 @@
-Remove-Item C:\Users\Admin\appdata\Local\nvim
-Remove-Item C:\Users\ADMIN\scoop\persist\win-vind\config\.vindrc
+Remove-Item $env:LOCALAPPDATA\nvim
+New-Item -ItemType SymbolicLink -Path $env:LOCALAPPDATA\nvim -Target $HOME\dotfiles\nvim\
 Remove-Item $PROFILE
+New-Item -ItemType SymbolicLink -Path $PROFILE -Target $HOME\dotfiles\Microsoft.PowerShell_profile.ps1
+#Remove-Item C:\Users\ADMIN\scoop\persist\win-vind\config\.vindrc
+#New-Item -ItemType SymbolicLink -Path C:\Users\ADMIN\scoop\persist\win-vind\config\.vindrc -Target C:\Home\dotfiles\.vindrc
 #Remove-Item $env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
-New-Item -ItemType SymbolicLink -Path C:\Users\Admin\appdata\Local\nvim -Target C:\Home\dotfiles\nvim\
-New-Item -ItemType SymbolicLink -Path C:\Users\ADMIN\scoop\persist\win-vind\config\.vindrc -Target C:\Home\dotfiles\.vindrc
-New-Item -ItemType SymbolicLink -Path $PROFILE -Target C:\Home\dotfiles\Microsoft.PowerShell_profile.ps1
 #New-Item -ItemType SymbolicLink -Path $env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json -Target C:\Home\dotfiles\wt\settings.json
