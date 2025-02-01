849d82a460b54df3cc82e6bd71e4a5f129cbf386
rfantic <nhnske@gmail.com>
2025-02-01 21:52:37 +0700

    update powershell profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,11 +1,7 @@
-clear
+#clear
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 function cdf {cd $(fd -L -t d | fzf)}
 function clipf {echo -n $pwd"\"$(fzf) | clip}
 function lsf {ls | Format-Wide -Column 4}
-function renplv {curl.exe -X POST -d '{}' https://api.netlify.com/build_hooks/652d0cf2603f7a2d000d1c00} 
-Set-Alias -n lg -v lazygit
-Set-Alias -n mm -v micromamba
-$Env:MAMBA_ROOT_PREFIX='C:\condaenv'
+#function renplv {curl.exe -X POST -d '{}' https://api.netlify.com/build_hooks/652d0cf2603f7a2d000d1c00} 
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
-Invoke-Expression (&starship init powershell)
