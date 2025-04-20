1d2a15718b5bc9c8fb73f8e16ee31b74404cb3f4
nghianh <nghianh@horusvn.com>
2025-04-20 15:25:30 +0700

    update for stow

--- .bashrc
+++ .bashrc
@@ -1,10 +1,2 @@
-# If not running interactively, don't do anything
-[[ $- != *i* ]] && return
-
-alias ls='ls --color=auto -X --group-directories-first'
-alias cdf='cd $(fd -L -t d | fzf)'
-
-eval "$(starship init bash)"
-
-. "$HOME/.cargo/env"
+eval "$(zoxide init bash)"
 
--- lnAll.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-ln -sf ~/dotfiles/nvim/ ~/.config/nvim
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/config.fish ~/.config/fish/config.fish
